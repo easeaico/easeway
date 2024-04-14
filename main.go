29da2df17c4385bf5f0b0785d0b2a28743418748
harry <yelcat@gmail.com>
2024-04-14 15:15:45 +0800

    add gemini pro api

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 	"fmt"
 
+	"github.com/easeaico/easeway/config"
+	"github.com/easeaico/easeway/gemini"
+	"github.com/easeaico/easeway/openai"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -13,14 +16,17 @@ func main() {
 	flag.StringVar(&confFile, "f", "config.yaml", "配置文件路径")
 	flag.Parse()
 
-	conf := NewConfig(confFile)
-	chatHandler := newChatCompletionHandler(&conf)
+	conf := config.NewConfig(confFile)
+	openaiClient := openai.NewOpenAIClient(&conf)
+	geminiClient := gemini.NewGenerativeAIClient(&conf)
+	defer geminiClient.Close()
 
 	e := echo.New()
 	// Root level middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	e.POST("/v1/chat/completions", chatHandler.HandleCompletions)
+	e.POST("/api/openai/v1/chat/completions", openaiClient.Handle)
+	e.POST("/api/openai/gemini/v1beta/models/:modelName\\:streamGenerateContent?key=:apiKey", geminiClient.Handle)
 
 	err := e.Start(fmt.Sprintf("%s:%d", conf.Server.IP, conf.Server.Port))
 	if err != nil {
